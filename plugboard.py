e37a94287b373c07f59e7c3e6a9580a006fdaf6c
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2021-10-19 00:20:42 -0400

    Created LetterSwitcher and Plugboard
    LetterSwitcher is a class for anything that can swap letters around
    It defines the format for storing letter mappings and functions for swapping letters
    
    Plugboard is a subclass of LetterSwitcher that is meant to function as the Plugboard of the virtual Enigma machine

---
+++ plugboard.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+from letterswitcher import LetterSwitcher, LettermapException
+
+class Plugboard(LetterSwitcher):
+
+	#allow for re-assigning lettermap after assignment
+	def setLettermap(self, lettermap):
+		
+		if not self.lettermapIsValid(lettermap):
+			raise LettermapException("Provided lettermap is invalid: {}".format(repr(lettermap)))
+		else:
+			self.lettermap = lettermap
+		
+if __name__ == '__main__':
+	
+	#test Plugboard
+	
+	lettermap = {
+		'a': 'n', 
+		'b': 'h', 
+		'c': 'a', 
+		'd': 'l', 
+		'e': 'g', 
+		'f': 'p', 
+		'g': 'u', 
+		'h': 'z', 
+		'i': 'q', 
+		'j': 'i', 
+		'k': 'v', 
+		'l': 'r', 
+		'm': 's', 
+		'n': 'd', 
+		'o': 'e', 
+		'p': 't', 
+		'q': 'x', 
+		'r': 'y', 
+		's': 'w', 
+		't': 'o', 
+		'u': 'b', 
+		'v': 'm', 
+		'w': 'c', 
+		'x': 'k', 
+		'y': 'f', 
+		'z': 'j'
+	}
+		
+	
+	encoder = Plugboard(Plugboard.getDefaultLettermap())
+	encoder.setLettermap(lettermap)
+	decoder = encoder.getDecoderInstance()
+	
+	msg = 'teststring'
+	
+	print(msg)
+	
+	encMsg = encoder.switchSequence(msg)
+	
+	print(encMsg)
+	
+	decMsg = decoder.switchSequence(encMsg)
+	
+	print(decMsg)
